259cd6eb989d8cb46e25c47b6d66a1f45ad33ee1
Jun Ito <jun_ito@voyagegroup.com>
2017-03-06 13:56:35 +0900

    Update: stereo cam

--- src/require.js
+++ src/require.js
@@ -1,3 +1,6 @@
 require('aframe')
-require('aframe-stereo-component')
+const stereoComponent = require('aframe-stereo-component').stereo_component;
+const stereocamComponent = require('aframe-stereo-component').stereocam_component;
 
+AFRAME.registerComponent('stereo', stereoComponent);
+AFRAME.registerComponent('stereocam', stereocamComponent);
